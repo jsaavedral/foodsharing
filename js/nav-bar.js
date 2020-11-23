7a28d27ab37184804cf7e5e09d696b6213ad8b35
Sander Möller <sander.mollerr@gmail.com>
2020-11-24 00:07:05 +0200

    Added redirection feature

---
+++ js/nav-bar.js
@@ -0,0 +1,8 @@
+document.getElementById("index").addEventListener("click", () => {
+    //window.location.replace("./index.html");
+    window.location.href = "index.html";
+});
+document.getElementById("about").addEventListener("click", () => {
+    //window.location.replace("./index.html");
+    window.location.href = "about.html";
+});
