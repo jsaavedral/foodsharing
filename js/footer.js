7d5f4dfdbe9da9076e7b3efb9e74e12ae5faa45b
Annaliis T <annaliis.tahevali@outlook.com>
2020-11-27 16:51:42 +0200

    lisasin footeri ikoonidele lingid

---
+++ js/footer.js
@@ -0,0 +1,12 @@
+document.getElementById("facebook").addEventListener("click", () => {
+    //window.location.replace("./index.html");
+    window.location.href = "https://www.facebook.com/foodsharingtartu";
+});
+document.getElementById("instagram").addEventListener("click", () => {
+    //window.location.replace("./index.html");
+    window.location.href = "https://www.instagram.com/foodsharingtartu/";
+});
+document.getElementById("twitter").addEventListener("click", () => {
+    //window.location.replace("./index.html");
+    window.location.href = "mailto:tartutoidujagamine@protonmail.com";
+});
